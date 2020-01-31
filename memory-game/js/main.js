52f5636f7bfd8d3f0845689495f0c7f722800766
Patrick McCaul <patrickmccaul@Patricks-MacBook-Pro.local>
2020-01-31 10:34:04 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,25 +1,24 @@
 let cards = ["Queen", "Queen", "King", "King"];
 // The user's current selection/hand
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
-
-// Prompt for user pick 1
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-// Prompt for user pick 2
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-// Check if user has selected 2 cards
-if (cardsInPlay.length === 2) {
+// 
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
 }
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
 
 /*
  Updates to game logic:

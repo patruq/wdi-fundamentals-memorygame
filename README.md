e6a7a6c7a4946cb978f3240f9ca9bce47ab57b6f
Patrick McCaul <patrickmccaul@Patricks-MacBook-Pro.local>
2020-02-04 10:33:58 -0500

    created game logic with reset button

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # wdi-fundamentals-memorygame
+
+Some notes on my approach to part 1 of the bonus problem of the pre-work project:
+/*
+	Add HTML button
+	Style with CSS
+	Create event in JS file that resets game after
+		playing (when button clicked)
+	-----
+	Create a clearBoard function
+	Then call createBoard
+	call cardElement and correct img, don't call flipboard?
+	Embed createBoard() into new function?
+	Mess with gameBoard?
+	Reset all global variables?
+	function resetEvent() {
+	let cards = [
+		{
+			rank: "Queen",
+			suit: "Hearts",
+			cardImage: "images/queen-of-hearts.png"
+		},
+		{
+			rank: "Queen",
+			suit: "Diamonds",
+			cardImage: "images/queen-of-diamonds.png"
+		},
+		{
+			rank: "King",
+			suit: "Hearts",
+			cardImage: "images/king-of-hearts.png"
+		},
+		{
+			rank: "King",
+			suit: "Diamonds",
+			cardImage: "images/king-of-diamonds.png"
+		}
+
+	];
+	let cardsInPlay = [];
+	createBoard();
+}
--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -62,11 +62,21 @@ function createBoard() {
 	};
 }
 
+function resetEvent() {
+	cardsInPlay = [];
+	document.getElementById("game-board").innerHTML = null;
+	createBoard();
+}
+
+
 createBoard();
 
-// let resentEvent = document.getElementById("resetButton");
-// resetEvent.addEventListener
 
+let clickReset = document.getElementById("resetButton");
+clickReset.addEventListener("click", resetEvent);
+
+
+	
 
 /*
 	Add HTML button
@@ -78,4 +88,34 @@ createBoard();
 	Then call createBoard
 	call cardElement and correct img, don't call flipboard?
 	Embed createBoard() into new function?
+	Mess with gameBoard?
+	Reset all global variables?
+	function resetEvent() {
+	let cards = [
+		{
+			rank: "Queen",
+			suit: "Hearts",
+			cardImage: "images/queen-of-hearts.png"
+		},
+		{
+			rank: "Queen",
+			suit: "Diamonds",
+			cardImage: "images/queen-of-diamonds.png"
+		},
+		{
+			rank: "King",
+			suit: "Hearts",
+			cardImage: "images/king-of-hearts.png"
+		},
+		{
+			rank: "King",
+			suit: "Diamonds",
+			cardImage: "images/king-of-diamonds.png"
+		}
+
+	];
+	let cardsInPlay = [];
+	createBoard();
+}
+
 */
